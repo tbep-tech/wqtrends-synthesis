31413fc53ec01c380f857a7957435502c5ab63ae
fawda123 <mbafs2012@gmail.com>
2021-12-16 10:39:06 -0500

    update data and model files to match those in SFbaytrends app

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,7 +2,15 @@ library(tidyverse)
 library(lubridate)
 library(sf)
 library(wqtrends)
-library(googlesheets4)
+
+# station lat/lon as separate file ----------------------------------------
+
+data(datprc)
+
+locs <- read.csv('raw/usgs_stations.csv') %>%
+  filter(Station %in% datprc$station)
+
+save(locs, file = 'data/locs.RData', compress = 'xz')
 
 # format raw wq data for use with wqtrends --------------------------------
 
@@ -11,7 +19,7 @@ chlraw <- read.csv('data/raw/sfb_surf_CB_SB_LSB.csv', stringsAsFactors = F)
 gppraw <- read.csv('data/raw/sfb_GPP_monthly.csv', stringsAsFactors = F) 
 doraw <- read.csv('data/raw/CB_SB_LSB_depthavg_O2.csv', stringsAsFactors = F)
 
-# get chlorophyll 
+# get chlorophyll as ug l-1
 chldat <- chlraw %>% 
   select(date, station, chl = chl_merge) %>% 
   gather('param', 'value', -date, -station) %>% 
@@ -23,28 +31,43 @@ chldat <- chlraw %>%
     mo = month(date, label = T),
     param = tolower(param)
   ) %>% 
-  filter(yr >= 1990 & yr <= 2019) %>% 
   filter(!is.na(value))
 
-# get gpp
+# get gpp as mg C m-2 d-1
 gppdat <- gppraw %>% 
- select(cont_year = dec_date, station, GPP) %>% 
- mutate(
-   date = date_decimal(cont_year),
-   date = as.Date(date),
-   doy = yday(date),
-   yr = year(date),
-   mo = month(date, label = T)
- ) %>% 
- rename(
-   gpp = GPP
- ) %>% 
- gather('param', 'value', gpp) %>% 
- select(date, station, param, value, doy, cont_year, yr, mo) %>% 
- filter(yr >= 1990 & yr <= 2019) %>% 
- filter(!is.na(value))
+  select(cont_year = dec_date, station, GPP) %>% 
+  mutate(
+    date = date_decimal(cont_year),
+    date = as.Date(date),
+    doy = yday(date),
+    yr = year(date),
+    mo = month(date, label = T)
+  ) %>% 
+  rename(
+    gpp = GPP
+  ) %>% 
+  gather('param', 'value', gpp) %>% 
+  select(date, station, param, value, doy, cont_year, yr, mo) %>% 
+  filter(!is.na(value))
 
-# get depht-averaged do
+# light attenuation as m-1
+kddat <- gppraw %>% 
+  select(cont_year = dec_date, station, ext_merge) %>% 
+  mutate(
+    date = date_decimal(cont_year),
+    date = as.Date(date),
+    doy = yday(date),
+    yr = year(date),
+    mo = month(date, label = T)
+  ) %>% 
+  rename(
+    kd = ext_merge
+  ) %>% 
+  gather('param', 'value', kd) %>% 
+  select(date, station, param, value, doy, cont_year, yr, mo) %>% 
+  filter(!is.na(value))
+
+# get depth-averaged do (mg/l and % sat)
 dodat <- doraw %>% 
   select(date, station, do, dosat = do_sat) %>% 
   gather('param', 'value', -date, -station) %>% 
@@ -56,12 +79,45 @@ dodat <- doraw %>%
     mo = month(date, label = T),
     param = tolower(param)
   ) %>% 
-  filter(yr >= 1990 & yr <= 2019) %>% 
   filter(!is.na(value))
 
 # combine new do ests, gpp with datprc
-datprc <- bind_rows(chldat, gppdat, dodat) %>% 
- arrange(station, param, date)
+datprc <- bind_rows(chldat, gppdat, dodat, kddat) %>% 
+  arrange(station, param, date) %>% 
+  filter(yr <= 2019)
+
+# find the most recent year with less than five observations
+# defaults to last year if all have at least five
+yrflt <- datprc %>% 
+  group_by(station, param, yr) %>% 
+  summarise(cnt = n(), .groups= 'drop') %>% 
+  group_by(station, param) %>% 
+  arrange(station, param, -yr) %>% 
+  mutate(
+    thrsh = cnt < 5, 
+    thrsh = cumsum(thrsh),
+    toflt = case_when(
+      thrsh == 1 & !duplicated(thrsh) ~ T,
+      sum(thrsh) == 0 ~ c(rep(NA, length(thrsh) -1), T)
+    )
+  ) %>% 
+  na.omit() %>% 
+  ungroup() %>% 
+  select(station, param, exclyr = yr)
+
+# join with min year for filter
+datprc <- datprc %>%
+  left_join(yrflt, by = c('station', 'param')) %>%
+  filter(yr > exclyr) %>%
+  select(-exclyr)
+
+# # combine new do ests, gpp with datprc
+# datprc <- bind_rows(chldat, gppdat, dodat) %>% 
+#   filter(yr >= 1990 & yr <= 2019) %>% 
+#   arrange(station, param, date)
+#
+# rawdat <- datprc
+# save(rawdat, file = '../wqtrends/data/rawdat.RData', compress = 'xz')
 
 save(datprc, file = 'data/datprc.RData', compress = 'xz')
 
@@ -72,12 +128,12 @@ data(datprc)
 # data to model, same as datprc, params in wide format, nested by station
 # crossed with frms
 tomod <- datprc %>% 
-  filter(param %in% c('chl', 'do', 'dosat', 'gpp')) %>% # add parameters here
+  filter(param %in% c('chl', 'do', 'dosat', 'gpp', 'kd')) %>% # add parameters here
   group_by(station, param) %>% 
   nest %>% 
   mutate(
     trans = case_when(
-      param %in% c('chl', 'gpp') ~ 'log10', 
+      param %in% c('chl', 'gpp', 'kd') ~ 'log10', 
       T ~ 'ident'
     )
   )
@@ -117,67 +173,3 @@ for(i in 1:nrow(tosv)){
   save(list = flnm, file = paste0('data/', flnm, '.RData'), compress = 'xz')
   
 }
-
-# get model estimates for synthesis indicators ----------------------------
-
-# # file with lookup combos
-# # https://docs.google.com/spreadsheets/d/13qYK33QKpfm6_jVKXB8J_T3Q8K8ut8lZUM1CTzXqQ5o/edit#gid=0
-# sht <- read_sheet('13qYK33QKpfm6_jVKXB8J_T3Q8K8ut8lZUM1CTzXqQ5o') %>% 
-#   select(var, metfun, doystr, doyend)
-
-# metric lookup table
-mets <- tibble(
-    param = c("chl", "chl", "gpp", "gpp", "dosat", "do", "gpp", "chl", "chl", "chl", "gpp", "gpp"), 
-    metfun = c("mean", "mean", "mean", "mean", "mean", "mean", "max", "max", "var", "var", "var", "var"), 
-    doystr = c(32, 213, 32, 213, 182, 182, 32, 32, 32, 152, 32, 152), 
-    doyend = c(121, 364, 152, 364, 305, 305, 152, 152, 152, 274, 152, 274)
-  )
-
-# estimates, takes about four hours to run
-metests <- list.files('data', pattern = '^mods', full.names = T) %>% 
-  tibble(
-    fl = .,
-    station = gsub('\\D+', '', .),
-    param = gsub('^data/mods_|[0-9]*|\\.RData$', '', .)
-  ) %>% 
-  left_join(mets, by = 'param') %>% 
-  group_by(fl) %>% 
-  nest() %>% 
-  mutate(
-    mod = purrr::map(fl, function(x){
-      
-      load(file = x)
-      
-      nm <- basename(x)
-      nm <- gsub('\\.RData', '', nm)
-      
-      out <- get(nm) %>% 
-        pull(modi) %>% 
-        deframe()
-      
-      return(out)
-      
-    })
-  ) %>% 
-  unnest(c('data')) %>% 
-  group_by(metfun, doystr, doyend, mod, fl) %>% 
-  nest() %>% 
-  mutate(
-    metseas = purrr::pmap(list(fl, metfun = metfun, mod = mod, doystr = doystr, doyend = doyend), function(fl, metfun, mod, doystr, doyend){
-      
-      cat(fl, metfun, '\n')
-      
-      toprs <- paste0('anlz_metseason(mod, ', metfun, ', ', doystr, ', ', doyend, ', nsim = 1e5, na.rm = TRUE)')
-      out <- eval(parse(text = toprs))
-    
-      return(out)
-      
-    })
-  ) %>% 
-  unnest('data') %>% 
-  ungroup() %>% 
-  select(-mod) %>% 
-  unnest('metseas') %>% 
-  select(station, param, metfun, doystr, doyend, yr, upr = bt_upr, lwr = bt_lwr, met = bt_met)
-
-write.csv(metests, 'results/metests.csv', row.names = F)
